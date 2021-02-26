0f044345b6e5aaf71b6b27ae5ec1b551b2c80e1f
dbeley <6568955+dbeley@users.noreply.github.com>
2021-02-26 19:14:35 +0100

    Update mplaylist.sh

--- mplaylist.sh
+++ mplaylist.sh
@@ -24,9 +24,14 @@ while read name; do
     track="${track#"${track%%[![:space:]]*}"}"
     # remove trailing whitespace characters
     track="${track%"${track##*[![:space:]]}"}"
-	printf "mpc search '((artist == \"$artist\") AND (title == \"$track\"))'\n"
-	mpc search "((artist == \"$artist\") AND (title == \"$track\"))"
+
+	potential_tracks = $(mpc search "((artist == \"$artist\") AND (title == \"$track\"))" )
+	if [[ $potential_tracks ]]; then
+    	echo "$potential_tracks" >> playlist2.txt
+	else
+		printf "mpc search '((artist == \"$artist\") AND (title == \"$track\"))'\n"
+	fi
+	# mpc search "((artist == \"$artist\") AND (title == \"$track\"))"
 	# result=$(mpc search "((artist == \"$artist\") AND (title == \"$track\"))")
-	mpc search "((artist == \"$artist\") AND (title == \"$track\"))" >> playlist2.txt
 
 done < "$FILE"
