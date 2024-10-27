e2635e8a90d8ebb91fdc046fafbd8cb31c662960
dbeley <6568955+dbeley@users.noreply.github.com>
2024-10-27 22:03:16 +0100

    Add script to extract jazz standards playlist

--- mpd_playlists.sh
+++ mpd_playlists.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 rsync -azvhP --stats --inplace --zc=zstd --zl=3 mpd_playlists/ ~/.config/mpd/playlists/
+rsync -azvhP --stats --inplace --zc=zstd --zl=3 jazz_standards/jazz_standards.m3u ~/.config/mpd/playlists/
--- navidrome_playlists.sh
+++ navidrome_playlists.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 rsync -azvhP --stats --inplace --zc=zstd --zl=3 playlists/ ~/nfs/WDC14/Musique/00_Playlists/
+rsync -azvhP --stats --inplace --zc=zstd --zl=3 jazz_standards/jazz_standards.m3u ~/nfs/WDC14/Musique/00_Playlists/
